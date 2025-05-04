070036fe285a5817dcf92df30607612350031efb
effbiae <effbiae@gmail.com>
2025-05-04 16:04:01 +1000

    comparing!

--- n.py
+++ n.py
@@ -1,8 +1,11 @@
 from a import P;import numpy as np
-ID=lambda x:x;ti=ID;te=ID
+ID=lambda x:x;ti=ID;te=ID;pk=ID
 Q="*%~"
 def m(c,x):
   if c=='*':return x*x
   if c=='%':return np.sqrt(x)
-  if c=='~':return np.vectorize(lambda x:0+(x!=0)(x)
-  return 0
+  if c=='~':return np.vectorize(lambda x:0+(x!=0))(x)
+  return None 
+def d(c,a,x):
+  if c=='*':return a*x
+  return None
--- t.py
+++ t.py
@@ -3,21 +3,21 @@ def e(x,m):
  if type(x)==float:return m.te(x)
  elif type(x)==int:return m.ti(x)
  elif len(x)==2:return m.m(x[0],e(x[1],m))
- else:return m.d(x[0],*[e(a,m)for a in x[1:]])
+ elif len(x)==3:return m.d(x[0],*[e(x,m)for x in x[1:]])
  return 0
 def ru(s):
- try:r=subprocess.run([f"python3","-c","from t import *;pk(e(p('{s}')),a)"],check=True)
+ try:r=subprocess.run([f"python3","-c","from t import *;a.pk(e(p('{s}'),a))"],check=True)
  except subprocess.CalledProcessError as err:
-  return err.returncode:
+  return err.returncode
  return r.returncode
 def cmp(x,y):return x==y
 if __name__=='__main__':
  s=[]
- for c in P[1:P.find('S')+1]:#first scan for SEGV
- print(c);d=repr(c)
- for a in [c+"(2)",c+"^2"]:
-  if x:=ru(s):print('SIGSEGV'if x==-11 else str(x),s)
-  else:s+=[a]
- for a in s:
-  if a[0] in n.Q:
-   print(cmp(*[x.pk(e(s,x))for x in (a,n)]))
+ for c in a.P[1:a.P.find('S')+1]:#first scan for SEGV
+  print(c);d=repr(c)
+  for t in [c+"(2)",c+"^2"]:
+   if x:=ru(t):print('SIGSEGV'if x==-11 else str(x),t)
+   else:s+=[t]
+ for x in s:
+  if x[0] in n.Q:
+   print(cmp(*[m.pk(e(x,m))for m in (a,n)]))
