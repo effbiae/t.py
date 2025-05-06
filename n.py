74fd37ca84f2dabdaee4bf88c4541e8fe1e97545
effbiae <effbiae@gmail.com>
2025-05-06 14:38:29 +1000

    tidy

--- n.py
+++ n.py
@@ -30,9 +30,7 @@ def d(c,a,x):
  if c=='=':return 0+(a==x)
  if c=='&':return np.minimum(a,x)
  if c=='|':return np.maximum(a,x)
- if c=='#':
-  n=m('#',x)
-  if ax(a) and n:return np.take(x,np.arange(a)%n)
+ if c=='#':n=m('#',x);return np.take(x,np.arange(a)%n) if ax(a)and n else None
  if c=='@':
   if not ax(a) and not ax(x):return np.matmul(a,x)
   if not ax(a):
