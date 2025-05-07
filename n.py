e504d31ada708c7e5b9a834058d86a16093666a7
effbiae <effbiae@gmail.com>
2025-05-07 20:40:16 +1000

    n enlist and catenate

--- n.py
+++ n.py
@@ -13,6 +13,8 @@ def m(c,x):
  if c=='_':return np.floor(x)
  if c=='|':return x[::-1]if not ax(x) else None
  if c=='!':return np.arange(int(x))if ax(x)else None
+ if c==',':return np.reshape(x,1)if ax(x)else np.matrix(np.reshape(x,(1,)+x.shape))if len(x.shape)==1 \
+                  else None
  if c=='@':
   try:return x[0]
   except TypeError:return x
@@ -30,6 +32,7 @@ def d(c,a,x):
  if c=='=':return 0+(abs(a-x)<1e-6)
  if c=='&':return np.minimum(a,x)
  if c=='|':return np.maximum(a,x)
+ if c==',':return np.concatenate((m(',',a)if ax(a)else a,m(',',x)if ax(x)else x))
  if c=='#':n=m('#',x);return np.take(x,np.arange(a)%n)if ax(a)and n else None
  if c=='@':
   if not ax(a)and not ax(x):return np.matmul(a,x)
