5d389e8487535e311ddf317f5f0daf4d6ae52ae5
effbiae <effbiae@gmail.com>
2025-05-07 15:16:18 +1000

    removed first pass, dumps c.b and es first up

--- t.py
+++ t.py
@@ -1,7 +1,7 @@
 import a;import n;from p import p;import numpy as np;import subprocess as sp
 import sys;import pickle;import os;P=a.P;Y=open('Y').read().split()
 ax=lambda x:not isinstance(x,np.ndarray);np.set_printoptions(precision=3)
-v='-v'in sys.argv;log=open('log','w');cf=open('c.b','w')
+v='-v'in sys.argv;lf,cf,ef=[open(x,'w')for x in['log','c.b','es']]
 def kb(f):
  try:f()
  except KeyboardInterrupt:pass
@@ -9,38 +9,28 @@ e=lambda x,m:([m.te,m.ti][type(x[1])==int](x[1]))if x[0]=='0'else \
         m.k(P.find(x[0]),e(x[1],m)if x[1] else None,e(x[2],m))
 ce=lambda x:f't{"ei"[type(x[1])==int]}({x[1]})'if x[0]=='0' else \
         f'k({P.find(x[0])},{ce(x[1])if x[1] else "0"},{ce(x[2])})'
-lg=lambda x,f:(print(x,file=f),f.flush());cb=lambda x:lg(f'_r({ce(p(x))});',cf)
-ev=lambda s,m:m.pk(e(p(s),m))
+lg=lambda x,f:(print(x,file=f),f.flush());cb=lambda x:[lg(f'_r({ce(p(a))});',cf)for a in x]
+le=lambda x:[lg(e,ef)for e in x];ev=lambda s,m:m.pk(e(p(s),m))
 def ru(s):
  try:r=sp.run(["python3","-c",f"from t import *;kb(lambda:ev('{s}',a))"],check=True)
  except sp.CalledProcessError as err:
   return err.returncode
  return r.returncode
-def safe():#returns exprs that don't segv
- s=[]
- print('testing for crash\n' +
-       ' this is slow because it runs a new k.edu process many times for each prim\n'+
-       '  be patient -- this is only done once and then cached')
- for c in a.P[1:a.P.find('S')+1]:#first scan for SEGV
-  print(c,end="");sys.stdout.flush()
-  g=('(2)','!2','^2','&2');n=lambda x,e:print('\na','SIGSEGV'if x==-11 else 'FAIL',e)
-  for t in g:
-   if x:=ru(e:=c+t):n(x,e)
-   else:
-    s+=[e]
-    for u in g:
-     if x:=ru(f:=f'({u}){c}{t}'):n(x,f)
-     else:s+=[f]
- print(' done');pickle.dump(s,open('s','wb'));return s
-chk=lambda x:lg(x,log)and 0
+def exprs():
+ g=('(2)','!2','^2','&2')
+ x=[(c,t)for t in g for c in a.P[1:a.P.find('S')+1]]
+ s=[f'{c}{t}'for (c,t) in x]
+ s+=[f'({u}){c}{t}'for u in g for (c,t) in x]
+ return s
+log=lambda x:lg(x,lf)
 def mis(x,s,t):
  sm=lambda an,x:[l:='\n'if (not ax(x))and len(x.shape)>1 else'',l+an+':'+l+str(x)][1]
  print('>>> mismatch:',x,sm('a',s),sm('n',t),'\n<<<')
 def match(x,y):c=(x-y)<1e-6;return c.all()if not ax(c)else c
 def main():
- for x in pickle.load(open('s','rb'))if os.path.exists('s')else safe():
-  if chk(x):continue
-  cb(x);v and print('try',x);s,t=[ev(x,m)for m in(a,n)]
+ es=exprs();cb(es);le(es)
+ for x in es:
+  log(x);v and print('try',x);s,t=[ev(x,m)for m in(a,n)]
   if type(s)is tuple:print('a','err',s[1],Y[s[1]],'for',x);continue
   elif t is None:v and print('n nyi',x);continue
   if not match(s,t):mis(x,s,t)
