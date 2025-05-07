e17e78e35d93ae09a202b6420a8f7d15992a68e4
effbiae <effbiae@gmail.com>
2025-05-07 19:59:51 +1000

    v and q sys.argv. tidy

--- t.py
+++ t.py
@@ -1,19 +1,13 @@
-import a;import n;from p import p;import numpy as np;import sys
-P=a.P;Y=open('Y').read().split()
+import a;import n;from p import p;import numpy as np;import sys;P=a.P;Y=open('Y').read().split()
 ax=lambda x:not isinstance(x,np.ndarray);np.set_printoptions(precision=3)
-lf,gf,ef=[open(x,'w')for x in['log','g.b','e']];v,q=['-'+x in sys.argv for x in 'vq']
-e=lambda x,m:([m.te,m.ti][type(x[1])==int](x[1]))if x[0]=='0'else \
-        m.k(P.find(x[0]),e(x[1],m)if x[1] else None,e(x[2],m))
-ce=lambda x:f't{"ei"[type(x[1])==int]}({x[1]})'if x[0]=='0'else \
-        f'k({P.find(x[0])},{ce(x[1])if x[1] else "0"},{ce(x[2])})'
+lf,gf,ef=[open(x,'w')for x in['log','g.b','e']];v,q=[x in sys.argv for x in 'vq']
+e=lambda x,m:([m.te,m.ti][type(x[1])==int](x[1]))if x[0]=='0'else m.k(P.find(x[0]),e(x[1],m)if x[1] else None,e(x[2],m))
+ce=lambda x:f't{"ei"[type(x[1])==int]}({x[1]})'  if x[0]=='0'else f'k({P.find(x[0])},{ce(x[1])if x[1] else "0"},{ce(x[2])})'
 lg=lambda x,f:(print(x,file=f),f.flush());gb=lambda x:[lg(f'_r({ce(p(a))});',gf)for a in x]
 le=lambda x:[lg(e,ef)for e in x];log=lambda x:lg(x,lf);ev=lambda s,m:m.pk(e(p(s),m))
-def exprs():
- g=('(2)','!2','^2','&2');x=[c+t for t in g for c in a.P[1:a.P.find('S')+1]]
- return x+[f'({u})'+a for u in g for a in x]
+exprs=lambda:(g:=('(2)','!2','^2','&2'),x:=[c+t for t in g for c in a.P[1:a.P.find('S')+1]],x+[f'({u})'+a for u in g for a in x])[-1]
 def mis(x,s,t):
- sm=lambda an,x:[l:='\n'if (not ax(x))and len(x.shape)>1 else'',l+an+':'+l+str(x)][1]
- print('>>> mismatch:',x,sm('a',s),sm('n',t),'\n<<<')
+ sm=lambda an,x:[l:='\n'if (not ax(x))and len(x.shape)>1 else'',l+an+':'+l+str(x)][1];print('>>> mismatch:',x,sm('a',s),sm('n',t),'\n<<<')
 def main():
  es=exprs();gb(es);le(es)
  for x in es:
