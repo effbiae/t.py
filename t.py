bd607d78470a18619f3609ee11abfe5a647ff2a2
effbiae <effbiae@gmail.com>
2025-05-07 15:24:46 +1000

    rename

--- t.py
+++ t.py
@@ -1,7 +1,7 @@
 import a;import n;from p import p;import numpy as np;import subprocess as sp
 import sys;import pickle;import os;P=a.P;Y=open('Y').read().split()
 ax=lambda x:not isinstance(x,np.ndarray);np.set_printoptions(precision=3)
-v='-v'in sys.argv;lf,cf,ef=[open(x,'w')for x in['log','g.b','es']]
+v='-v'in sys.argv;lf,gf,ef=[open(x,'w')for x in['log','g.b','e']]
 def kb(f):
  try:f()
  except KeyboardInterrupt:pass
@@ -9,7 +9,7 @@ e=lambda x,m:([m.te,m.ti][type(x[1])==int](x[1]))if x[0]=='0'else \
         m.k(P.find(x[0]),e(x[1],m)if x[1] else None,e(x[2],m))
 ce=lambda x:f't{"ei"[type(x[1])==int]}({x[1]})'if x[0]=='0' else \
         f'k({P.find(x[0])},{ce(x[1])if x[1] else "0"},{ce(x[2])})'
-lg=lambda x,f:(print(x,file=f),f.flush());cb=lambda x:[lg(f'_r({ce(p(a))});',cf)for a in x]
+lg=lambda x,f:(print(x,file=f),f.flush());gb=lambda x:[lg(f'_r({ce(p(a))});',gf)for a in x]
 le=lambda x:[lg(e,ef)for e in x];log=lambda x:lg(x,lf);ev=lambda s,m:m.pk(e(p(s),m))
 def ru(s):
  try:r=sp.run(["python3","-c",f"from t import *;kb(lambda:ev('{s}',a))"],check=True)
@@ -27,7 +27,7 @@ def mis(x,s,t):
  print('>>> mismatch:',x,sm('a',s),sm('n',t),'\n<<<')
 def match(x,y):c=(x-y)<1e-6;return c.all()if not ax(c)else c
 def main():
- es=exprs();cb(es);le(es)
+ es=exprs();gb(es);le(es)
  for x in es:
   log(x);v and print('try',x);s,t=[ev(x,m)for m in(a,n)]
   if type(s)is tuple:print('a','err',s[1],Y[s[1]],'for',x);continue
