2e2f60bab53b580be3475767a2d9fadccb74ae20
effbiae <effbiae@gmail.com>
2025-05-07 17:05:06 +1000

    tidy

--- n.py
+++ n.py
@@ -17,7 +17,7 @@ def m(c,x):
   except TypeError:return x
  if c=='^':
   if type(x)==int:return 1/x*np.arange(x)
- if c=='&':return np.full((x,x),1,np.dtype('b'))if ax(x) else np.transpose(x)
+ if c=='&':return np.full((x,x),1,np.dtype('b'))if ax(x)else np.transpose(x)
  return None 
 def d(c,a,x):
  if a is None or x is None:return None
@@ -25,18 +25,15 @@ def d(c,a,x):
  if c=='-':return a-x
  if c=='*':return a*x
  if c=='%':return a/x
- if c=='!':return a//x
  if c=='<':return 0+(a<x)
  if c=='>':return 0+(a>x)
  if c=='=':return 0+(a==x)
  if c=='&':return np.minimum(a,x)
  if c=='|':return np.maximum(a,x)
- if c=='#':n=m('#',x);return np.take(x,np.arange(a)%n) if ax(a)and n else None
+ if c=='#':n=m('#',x);return np.take(x,np.arange(a)%n)if ax(a)and n else None
  if c=='@':
-  if not ax(a) and not ax(x):return np.matmul(a,x)
-  if not ax(a):
-   try:return a[x]
-   except IndexError:pass
+  if not ax(a)and not ax(x):return np.matmul(a,x)
+  if not ax(a)and x<len(a):return a[x]
   return a*x
  if c=='~':c=(a-x)<1e-6;return int(c if ax(c)else c.all())
  return None 
