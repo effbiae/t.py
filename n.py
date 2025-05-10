a962a512de39f974f0d006eb7e557d5a6740026b
effbiae <effbiae@gmail.com>
2025-05-10 17:32:41 +1000

    made n fit a more

--- n.py
+++ n.py
@@ -6,7 +6,7 @@ k=lambda i,a,x:topy(m(P[i],x)if a is None else d(P[i],a,x))
 def tr(f,v,e):
  try:return f(*v)
  except:return e
-Y='nyi 1 2 3 4 5 6 7 wontdo err'.split()
+Y='nyi rnk 2 typ 4 5 6 7 wontdo err'.split()
 def m(c,x):
  if c=='?':return (8,)
  if c=='+':return abs(x)
@@ -17,11 +17,14 @@ def m(c,x):
  if c=='*':return x*x
  if c=='~':return d('=',x,0)
  if c=='_':return np.floor(x)
- if c=='|':return x[::-1]if not ax(x) else (9,)
+ if c=='|':return x[::-1]if not ax(x) else m('|',np.identity(x))
+ if c=='<':return np.triu(np.ones((x,x)),+1)if ax(x) else (9,)
+ if c=='>':return np.tril(np.ones((x,x)),-1)if ax(x) else (9,)
+ if c=='=':return np.identity(x)if ax(x) else (9,)
  if c=='!':return np.arange(int(x))if ax(x)else (9,)
  if c==',':return np.reshape(x,1)if ax(x)else np.reshape(x,(1,)+x.shape)if len(x.shape)==1 else (9,)
  if c=='@':return tr(lambda x:x[0],(x,),x)
- if c=='^':return 1/x*np.arange(x)if type(x)==int else (9,)
+ if c=='^':return 1/x*np.arange(x)if type(x)==int else(9,)
  if c=='&':return np.full((x,x),1)if ax(x)else np.transpose(np.matrix(x))
  return (0,) 
 def d(c,a,x):
@@ -37,16 +40,20 @@ def d(c,a,x):
   q=x/a;return q if any([ty(_)==5 for _ in (a,x)]) else np.floor(q)
  if c=='&':return np.minimum(a,x)
  if c=='|':return np.maximum(a,x)
- if c=='_':return tr(lambda a,x:x[a:],(a,x),(0,))
+ if c=='_':return tr(lambda a,x:x[a:],(a,x),(1,))
  if c==',':return tr(lambda a,x:np.concatenate([m(',',_)if ax(_)else _ for _ in(a,x)]),(a,x),(0,))
- if c=='#':n=m('#',x);return np.take(x,np.arange(a)%n)if ax(a)and n else (9,)
+ if c=='#':n=m('#',x);return np.take(x,np.arange(a)%n)if ax(a)and n else (1,)
  if c=='@':#s@ is scalar(i.e. multiply);v@ is index;m@ is matmul
   if ax(a):return a*x
-  if len(a.shape)==1:return tr(lambda a,x:a[x],(a,x),(9,)) #if ty(x)==2:
+  if len(a.shape)==1:return tr(lambda a,x:a[x],(a,x),(9,))if ty(x)==2 else(3,)
   if len(a.shape)==2:return tr(np.matmul,(a,x),(9,))
  if c=='~':
      if ax(a)!=ax(x):return 0
      p=[m(',',_)if ax(_)else _ for _ in(a,x)]
      if not np.equal(*[_.shape for _ in p]).all():return 0
      return 0+d('|',np.logical_and(*[np.isnan(_)for _ in p]),d('=',*p)).all()
+ if c=='^':
+     if ty(a)!=2:return (1,)
+     f=lambda a,x:m('&',d('#',a,x)if ax(a)else a);x=m('!',x)if ax(x)else x
+     return tr(f,(a,x),(9,))if ax(a)else(1,)
  return (0,)
--- t
+++ t
@@ -8,8 +8,8 @@ ce=lambda x:f't{"ei"[type(x[1])==int]}({x[1]})'  if x[0]=='0'else f'ke({P.find(x
 lg=lambda x,f:(print(x,file=f),f.flush());cb=lambda x:[lg(f'_r({ce(p(a))}); //{a}',gf)for a in x]
 le=lambda x:[lg(e,ef)for e in x];log=lambda x:lg(x,lf);ev=lambda s,m:m.pk(e(p(s),m))
 exprs=lambda:(g:=('(2)','!2','^2','&2'),x:=[c+t for t in g for c in a.P[1:a.P.find('.')+1]],x+[f'({u})'+a for u in g for a in x])[-1]
-sm=lambda an,x:[l:='\n'if (not ax(x))and len(x.shape)>1 else'',l+an+':'+l+str(x)][1];
-err=lambda x:type(x)is tuple;errpass=lambda s,e:e==8 or err(s) and s[0]in[0,1]
+sm=lambda an,x:[l:='\n'if (not ax(x))and len(x.shape)>1 else'',l+an+':'+l+(str(x)if not err(x)else 'err '+str(x[0]))][1];
+err=lambda x:type(x)is tuple;errpass=lambda s,e:e==8 or err(s) and (s[0]in[0]or s[0]==e)
 def help():p=sys.argv[0];print(f'try {p} for differences, then try {p} n for possible bugs. also {p} a \'2+^2\' for single expr')
 def main(es):
  cb(es);le(es)
