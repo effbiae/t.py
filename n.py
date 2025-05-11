76f77597582da4a4b31241a42bebfb24f9086f53
effbiae <effbiae@gmail.com>
2025-05-11 17:15:27 +1000

    confirmed atws fixes done except !!2

--- n.py
+++ n.py
@@ -18,7 +18,7 @@ def m(c,x):
  if c=='*':return x*x
  if c=='~':return d('=',x,0)
  if c=='_':return np.floor(x)
- if c=='|':return x[::-1]if not ax(x) else m('|',np.identity(x))
+ if c=='|':return x[::-1]if not ax(x) and len(x.shape)==1 else(1,)
  if c=='<':return np.triu(np.ones((x,x)),+1)if ax(x) else (9,)
  if c=='>':return np.tril(np.ones((x,x)),-1)if ax(x) else (9,)
  if c=='=':return np.identity(x)if ax(x) else (9,)
@@ -26,7 +26,7 @@ def m(c,x):
  if c==',':return np.reshape(x,1)if ax(x)else np.reshape(x,(1,)+x.shape)if len(x.shape)==1 else (9,)
  if c=='@':return tr(lambda x:x[0],(x,),x)
  if c=='^':return 1/x*np.arange(x)if type(x)==int else(9,)
- if c=='&':return np.full((x,x),1)if ax(x)else np.transpose(np.matrix(x))
+ if c=='&':return np.full((x,x),1)if ax(x)else np.transpose(np.matrix(x))if (len(x.shape)==2)else (1,)
  return (0,) 
 def d(c,a,x):
  if c=='+':return a+x
@@ -37,10 +37,28 @@ def d(c,a,x):
  if c=='>':return 0+(a>x)
  if c=='=':return 0+(abs(a-x)<1e-6)
  if c=='!':return x%a if ty(x)==2 else a*x
- if c=='?':#s? is inverse;v? is inverse;m? is commute
-  if not ax(a) and len(a.shape)==1:
-   i=np.nonzero([d('~',a[i],x)for i in np.arange(len(a))])[0]
-   return i if len(i) else len(a)
+ if c=='?':#s? is inverse;v? is inverse;m? is commutem (x@m)
+   '''m?x is x@m (i.e. commute of @. useful for our ai machinations)
+btw in general  a@x is at/application/composition/..
+i.e. 'a' is a function. e.g. scalar s is scalar i.e. it scales. that is why it is called scalar.
+2@3 is 6
+
+a?x is the inverse operation, e.g
+
+2?6 is 3
+
+except m?x is not least squares. instead we are using it for something else.
+
+m@x is linear transform(m is function)
+v@x is indexing (vectors are functions)
+v?x is find(i.e. inverse of index)
+  '''
+  if not ax(a):
+   if not ax(x):return(3,)
+   if len(a.shape)==1:
+    i=np.nonzero([d('~',a[i],x)for i in np.arange(len(a))])[0]
+    return i if len(i)else len(a)
+   else:return(2,)
   q=x/a;return q if any([ty(_)==5 for _ in (a,x)]) else np.floor(q)
  if c=='&':return np.minimum(a,x)
  if c=='|':return np.maximum(a,x)
@@ -51,7 +69,7 @@ def d(c,a,x):
  if c=='#':n=m('#',x);return (m(',',x)if ax(x) else x)[np.arange(a)%n]if ax(a)and n else (1,)
  if c=='@':#s@ is scalar(i.e. multiply);v@ is index;m@ is matmul
   if ax(a):return a*x
-  if len(a.shape)==1:return tr(lambda a,x:a[x],(a,x),(9,))if ty(x)==2 else(3,)
+  if len(a.shape)==1:return tr(lambda a,x:a[x],(a,x),(9,))if ax(x)and ty(x)==2 else(3,)
   if len(a.shape)==2:return tr(np.matmul,(a,x),(9,))
  if c=='~':
      if ax(a)!=ax(x):return 0
