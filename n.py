11877543f55cf79d9f9d0ff4193f816f5cdb609b
effbiae <effbiae@gmail.com>
2025-05-11 21:39:05 +1000

    introduce mx, vx

--- n.py
+++ n.py
@@ -2,7 +2,7 @@ from a import P;import numpy as np;np.seterr(divide='ignore', invalid='ignore');
 ax=lambda x:not isinstance(x,np.ndarray)or not len(x.shape);ID=lambda x:x;ti=ID;te=ID;pk=ID
 ty=lambda x:2 if type(x)is int else 5 if type(x)is float else [5,2][0+(x.dtype=='int64')]
 topy=lambda x:x.item()if not ax(x)and x.shape==()else x;k=lambda i,a,x:topy(m(P[i],x)if a is None else d(P[i],a,x))
-mv=lambda n:lambda x:not ax(x)and len(x.shape)==n;v=mv(1);m=mv(2)
+mv=lambda n:lambda x:not ax(x)and len(x.shape)==n;vx=mv(1);mx=mv(2)
 def tr(f,v,e):
  try:return f(*v)
  except:return e
@@ -10,15 +10,15 @@ Y='nyi rnk len typ wontdo other'.split()
 def m(c,x):
  if c in'?+-*%#*~_':return(4,)if c=='?'else abs(x)if c=='+'else -x if c=='-'else x*x if c=='*'else \
   np.sqrt(x)if c=='%'else(1 if ax(x)else len(x))if c=='#'else x*x if c=='*'else d('=',x,0)if c=='~'else np.floor(x)
- if c=='|':return x[::-1]if v(x)else np.identity(x)[::-1]if ax(x)else(1,)
+ if c=='|':return x[::-1]if vx(x)else np.identity(x)[::-1]if ax(x)else(1,)
  if c=='<':return np.triu(np.ones((x,x),dtype=int),+1)if ax(x)else(5,)
  if c=='>':return np.tril(np.ones((x,x),dtype=int),-1)if ax(x)else(5,)
  if c=='=':return np.identity(x)if ax(x)else(5,)
  if c=='!':return np.arange(int(x))if ax(x)else np.array(x.shape)
- if c==',':return np.reshape(x,1)if ax(x)else np.reshape(x,(1,)+x.shape)if len(x.shape)==1 else(1,)
+ if c==',':return np.reshape(x,1)if ax(x)else np.reshape(x,(1,)+x.shape)if vx(x)else(1,)
  if c=='@':return tr(lambda x:x[0],(x,),x)
  if c=='^':return 1/x*np.arange(x)if type(x)==int else(5,)
- if c=='&':return np.full((x,x),1)if ax(x)else np.transpose(np.matrix(x))if(len(x.shape)==2)else(1,)
+ if c=='&':return np.full((x,x),1)if ax(x)else np.transpose(np.matrix(x))if mx(x)else(1,)
  return(0,)
 def d(c,a,x):
  if c in'+-*%<>=!&|':return a+x if c=='+'else a-x if c=='-'else a*x if c=='*'else (np.array(float(a))if ax(a)else a)/x if c=='%' \
@@ -26,10 +26,10 @@ def d(c,a,x):
   else np.minimum(a,x)if c=='&'else np.maximum(a,x)
  if c=='?':#s? is inverse;v? is inverse;m? is commutem (x@m)
   if not ax(a):
-   if len(a.shape)==2:
+   if mx(a):
     if ty(x)!=2:return(2,)
    if not ax(x):return(3,)
-   if len(a.shape)==1:
+   if vx(a):
     i=np.nonzero([d('~',a[i],x)for i in np.arange(len(a))])[0]
     return i if len(i)else len(a)
    else:return(2,)
@@ -41,10 +41,10 @@ def d(c,a,x):
  if c=='#':n=m('#',x);return(m(',',x)if ax(x)else x)[np.arange(a)%n]if ax(a)and n else(1,)
  if c=='@':#s@ is scalar(i.e. multiply);v@ is index;m@ is matmul
   if ax(a):return a*x
-  if len(a.shape)==1:
+  if vx(a):
    if ty(x)==2:return tr(lambda a,x:a[x],(a,x),(2,))if ax(x)else(2,)
    else:return(3,)
-  if len(a.shape)==2:return tr(np.matmul,(a,x),(5,))
+  if mx(a):return tr(np.matmul,(a,x),(5,))
  if c=='~':
      if ax(a)!=ax(x):return 0
      p=[m(',',_)if ax(_)else _ for _ in(a,x)]
