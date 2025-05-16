fcb6e8c6feb41e067b32a5c25495a1a34b3e6252
effbiae <effbiae@gmail.com>
2025-05-16 18:24:37 +1000

    fix leak in ke

--- a.c
+++ a.c
@@ -1,9 +1,9 @@
 #include"k.edu/z.a"
-void ldmxcsr(){o(ia32_ldmxcsr)(0xbfc0);}
 U kti(i2 x){return ti(x);}
 U kte(e2 x){return te(x);}
 #define xx(s) i2 k##s##x(Ux){return s##x;}
 xx(n)xx(t)xx(i)xx(m)xx(a)xx(p)xx(u)
 e2 kex(Ux){return ex;}
 i0 kgx(Ux){return (i0)ix;}
-U k(i2,U,U);U ke(ii,Ua,Ux){U r;return a&&pa?a:px?x:k(i,a,x);}
+U k_(i2,U);static U _r(U x){return px||ax||k_(0,x);}
+U k(i2,U,U);U ke(ii,Ua,Ux){return a&&pa?_r(x),a:px?_r(a),x:k(i,a,x);}
