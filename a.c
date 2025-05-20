0591b53fbf0c595d78dcf238535b05aa491fed6b
effbiae <effbiae@gmail.com>
2025-05-20 17:50:17 +1000

    adapt to new k.edu and tidy

--- a.c
+++ a.c
@@ -1,5 +1,5 @@
-#include"k.edu/_.b"
 #define sx ((i0*)k_(0,x))
+#include"k.edu/_.b"
 #include"k.edu/z.a"
 U k_r(Ux){return px||ax?x:k(0,x,0);}
 U kr_(Ux){return px||ax?x:k(0,0,x);}
--- g.c
+++ g.c
@@ -1,5 +1,8 @@
-#include"k.edu/z.b"
-U ke(U,U,U);
+#include"k.edu/a.a"
+#include"k.edu/_.b"
+U k_(i2,U),ke(U,U,U),k_r(U),kr_(U);
+#define r_ kr_
+#define _r k_r
 int main(){
 	static U a,b,c,d,e,f,g,h,i,j,l,m,n,o,p,q,r,s,t,v,w,x,y,z;
 	k_(0,0);
