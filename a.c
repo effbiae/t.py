2684152d39c91053296d2f5b0e713e868a2b83c0
effbiae <effbiae@gmail.com>
2025-05-25 13:18:01 +1000

    update to newest k.edu

--- a.c
+++ a.c
@@ -1,8 +1,7 @@
 #define sx ((i0*)k_(0,x))
-#include"k.edu/_.b"
-#include"k.edu/z.a"
-U k_r(Ux){return px||ax?x:k(0,x,0);}
-U kr_(Ux){return px||ax?x:k(0,0,x);}
+#include"k.edu/a.h"
+U k_r(Ux){return px||ax?x:_r(x);}
+U kr_(Ux){return px||ax?x:r_(x);}
 U kti(i2 x){return ti(x);}
 U kte(e2 x){return te(x);}
 #define xx(s) i2 k##s##x(Ux){return s##x;}
@@ -10,4 +9,5 @@ xx(n)xx(t)xx(i)xx(m)xx(a)xx(p)xx(u)
 i0*ksx(Ux){return sx;}
 e2 kex(Ux){return ex;}
 i0 kgx(Ux){return (i0)ix;}
-U ke(U i,Ua,Ux){return a&&pa?k_r(x),a:px?k_r(a),x:k(i,a,x);}
+static U kk(U i,Ua,Ux){return a&&pa?k_r(x),a:px?k_r(a),x:k(i,a,x);}
+U ke(U i,Ua,Ux){return r(kk(i,a,x),if(pr)(k_r(a),k_r(x)));}
--- a.py
+++ a.py
@@ -1,7 +1,7 @@
 import ctypes as ct;U=ct.c_ulonglong;i2=ct.c_uint;e2=ct.c_float;i0=ct.c_uint8;P=open("P").read();a=ct.CDLL('./liba.so')
 def d(f,s):f.restype=s[0];f.argtypes=s[1:];return f
-k_=d(a.k_,[U,i2,U]);ti=d(a.kti,[U,i2]);te=d(a.kte,[U,e2]);ix=d(a.kix,[ct.c_int,U]);ex=d(a.kex,[e2,U])
-a_ke=d(a.ke,[U,U,U,U]);k=lambda i,x,y:a_ke(i,0 if x is None else x,y)
+k_=d(a.k_,[U,U]);m_=d(a.m_,[U,i2,U]);ti=d(a.kti,[U,i2]);te=d(a.kte,[U,e2]);ix=d(a.kix,[ct.c_int,U]);ex=d(a.kex,[e2,U])
+a_ke=d(a.ke,[U,U,U,U]);k=lambda i,x,y:a_ke(i-1,0 if x is None else x,y)
 ax=d(a.kax,[i2,U]);tx=d(a.ktx,[i2,U]);mx=d(a.kmx,[i2,U]);nx=d(a.knx,[i2,U]);gx=d(a.kgx,[i0,U]);px=d(a.kpx,[i2,U]);ux=d(a.kux,[i2,U])
 _r=d(a.k_r,[U,U]);r_=d(a.kr_,[U,U]);sx=d(a.ksx,[U,U])
 sh=lambda x:0 if ax(x)else(ux(x),round(nx(x)/ux(x)if ux(x)else nx(x)))if mx(x)else(nx(x),)
@@ -12,4 +12,4 @@ g=lambda x,t,s:[np.copy(np.ctypeslib.as_array(ct.cast(sx(x),ct.POINTER(t)),shape
 u=lambda a:np.unpackbits(a,bitorder='little')
 def pk(x):
     return (ix(x),)if px(x)else a(x)if ax(x)else g(x,ty(x),sh(x))if tx(x)!=b else u(g(x,ty(x),((nx(x)+7)//8,)))[:nx(x)].reshape(sh(x))
-k_(0,0)
+k_(m_(0,2**36))
