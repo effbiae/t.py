2f9a9881c4da475973ed6d41b3033d622bf6c461
effbiae <effbiae@gmail.com>
2025-06-08 13:47:50 +1000

    use vz in matrix shape

--- a.c
+++ a.c
@@ -7,7 +7,7 @@ U kr_(Uz){return pz||az?z:r_(z);}
 U kti(i2 z){return ti(z);}
 U kte(e2 z){return te(z);}
 #define zz(s) i2 k##s##z(Uz){return s##z;}
-zz(n)zz(t)zz(i)zz(m)zz(a)zz(p)zz(u)
+zz(n)zz(t)zz(i)zz(m)zz(a)zz(p)zz(v)zz(u)
 e2 kez(Uz){return ez;}
 i0 kgz(Uz){return (i0)iz;}
 U ke(U i,Uy,Uz){return y&&py?k_r(z),y:pz?k_r(y),z:r(k(i,y,z),if(z(r,pz))(k_r(y),k_r(z)));}
--- a.py
+++ a.py
@@ -2,10 +2,10 @@ import ctypes as ct;from numpy import *;U=ct.c_ulonglong;i2=ct.c_uint;e2=ct.c_fl
 def d(f,s):f.restype=s[0];f.argtypes=s[1:];return f
 k_=d(a.k_,[U,U,U]);m_=d(a.m_,[U,i2,U]);ti=d(a.kti,[U,i2]);te=d(a.kte,[U,e2]);iz=d(a.kiz,[ct.c_int,U]);ez=d(a.kez,[e2,U])
 a_ke=d(a.ke,[U,U,U,U]);k=lambda i,z,y:a_ke(i-1,0 if z is None else z,y);_r=d(a.k_r,[U,U]);r_=d(a.kr_,[U,U])
-az=d(a.kaz,[i2,U]);tz=d(a.ktz,[i2,U]);mz=d(a.kmz,[i2,U]);nz=d(a.knz,[i2,U]);gz=d(a.kgz,[i0,U]);pz=d(a.kpz,[i2,U]);uz=d(a.kuz,[i2,U])
+az=d(a.kaz,[i2,U]);tz=d(a.ktz,[i2,U]);mz=d(a.kmz,[i2,U]);nz=d(a.knz,[i2,U]);gz=d(a.kgz,[i0,U]);pz=d(a.kpz,[i2,U]);uz=d(a.kuz,[i2,U]);vz=d(a.kvz,[i2,U])
 i,f,b,q=tz(ti(1)),tz(te(1.)),1,4;ty=lambda t:ct.c_int if t==i else e2 if t==f else i0 if t==b else i0 if t==q else i0
 ndt=lambda t:ctypeslib.ndpointer(ty(t),flags="C_CONTIGUOUS");kce=d(a.kce,[U,ndt(f),U]);kci=d(a.kci,[U,ndt(i),U])
-sh=lambda z:0 if az(z)else(uz(z),round(nz(z)/uz(z)if uz(z)else nz(z)))if mz(z)else(nz(z),)
+sh=lambda z:0 if az(z)else(uz(z),vz(z))if mz(z)else(nz(z),)
 err=lambda z:pz(z);a=lambda z:array(iz(z)if tz(z)==i else ez(z) if tz(z)==f else gz(z))
 g=lambda z,t,s:[(kce if t==f else kci)(a:=ones(s,dtype=float32 if t==f else int32),z),_r(z),a][-1]
 pk=lambda z:(iz(z),)if pz(z)else a(z)if az(z)else g(z,tz(z),sh(z))
